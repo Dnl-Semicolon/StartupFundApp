7da9664ddb3df42109d00bfc65cbf3af54e39483
Daniel Yee Kheng Tan <danielykt-pm23@student.tarc.edu.my>
2026-04-13 12:41:22 +0800

    deployed contracts

---
+++ src/lib/contractAddresses.ts
@@ -0,0 +1,10 @@
+export const CONTRACT_ADDRESSES = {
+  startupFund:     "0x5188ab1d7947b3fb8b379681804f58236273de92",
+  campaignManager: "0x7e04bce952455095f8047b0e2bde9d743bd6b07a",
+  fundingVault:    "0x47d323a1b5a5eb09671ca34fc07a1b0a5c36b450",
+  rewardToken:     "0xf6abfbe0c6d4f672d5ae922a20608236b5269096",
+  accessControl:   "0x0e49ee16b24e91c4feb4e5a729e881cbc618164f",
+};
+
+// Chain ID for Ganache
+export const CHAIN_ID = 1337;
